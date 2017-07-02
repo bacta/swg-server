038378c05e64247a575a8df7cfba21f7ccec1ae0
Chris Rush <chris@crrush.com>
2017-07-02 13:10:56 -0600

    Changed prefix of login-server properties. Added enableAutoGalaxyRegistration property. Added root readme. Added some chat server stuff.

--- chat-shared/src/main/java/com/ocdsoft/bacta/chat/service/InstantMessageService.java
+++ chat-shared/src/main/java/com/ocdsoft/bacta/chat/service/InstantMessageService.java
@@ -1,11 +1,11 @@
 package com.ocdsoft.bacta.chat.service;
 
 import com.ocdsoft.bacta.chat.ChatAvatarId;
-import com.ocdsoft.bacta.chat.ChatError;
+import com.ocdsoft.bacta.chat.ChatResult;
 
 /**
  * Created by crush on 6/25/2017.
  */
 public interface InstantMessageService {
-    ChatError sendInstantMessage(ChatAvatarId from, ChatAvatarId to, String message, String outOfBand);
+    ChatResult sendInstantMessage(ChatAvatarId from, ChatAvatarId to, String message, String outOfBand);
 }
