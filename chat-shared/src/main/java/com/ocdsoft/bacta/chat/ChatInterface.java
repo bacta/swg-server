e7f0f5507ad831d47d49378d52cd3a4e74bfa94e
Chris Rush <chris@crrush.com>
2017-07-02 13:10:56 -0600

    Changed prefix of login-server properties. Added enableAutoGalaxyRegistration property. Added root readme. Added some chat server stuff.

--- chat-shared/src/main/java/com/ocdsoft/bacta/chat/ChatInterface.java
+++ chat-shared/src/main/java/com/ocdsoft/bacta/chat/ChatInterface.java
@@ -2,10 +2,37 @@ package com.ocdsoft.bacta.chat;
 
 /**
  * Created by crush on 6/28/2017.
- *
- * The chat interface is an expansion point that can define additional behavior between
- * a galaxy chat service, and some secondary service. For example, it could defer logic
- * to an external chat server.
+ * <p>
+ * Defines how the galaxy chat service communicates with a chat server authority. A chat server authority may be a
+ * remote chat server like an XMPP based server, IRC, Slack, etc. The default implementation provided with Bacta is
+ * {@link LocalChatInterface} which handles all chat operations locally, and functions mainly as a dummy chat
+ * server. It is recommended for local development or small-scale LAN based servers with only a single galaxy cluster.
  */
 public interface ChatInterface {
+    /**
+     * Attempts to log the avatar into the chat server. Expected to return {@link ChatResult#SUCCESS} if the login
+     * was successful.
+     *
+     * @param characterName The name of the character logging into the chat server.
+     * @param networkId     The id of the character logging into the chat server.
+     * @return {@link ChatResult#SUCCESS} if login was successful. Should not be null.
+     */
+    ChatResult loginAvatar(String characterName, long networkId);
+
+    /**
+     * Attempts to log the avatar out of the chat server. Expected to return {@link ChatResult#SUCCESS} if the logout
+     * was successful.
+     *
+     * @param avatarId The avatar that is being logged out of the chat server.
+     * @return {@link ChatResult#SUCCESS} if logout was successful. Should not be null.
+     */
+    ChatResult logoutAvatar(ChatAvatarId avatarId);
+
+    /**
+     * Attempts to destroy an avatar on the authoritative chat server.
+     *
+     * @param avatarId
+     * @return
+     */
+    ChatResult destroyAvatar(ChatAvatarId avatarId);
 }
--- chat-shared/src/main/java/com/ocdsoft/bacta/chat/service/AvatarService.java
+++ chat-shared/src/main/java/com/ocdsoft/bacta/chat/service/AvatarService.java
@@ -1,7 +1,5 @@
 package com.ocdsoft.bacta.chat.service;
 
-import com.ocdsoft.bacta.chat.AvatarDoesNotExistException;
-import com.ocdsoft.bacta.chat.AvatarNotConnectedException;
 import com.ocdsoft.bacta.chat.ChatAvatarId;
 
 /**
@@ -12,52 +10,33 @@ import com.ocdsoft.bacta.chat.ChatAvatarId;
  * services rely on the AvatarService for resolving networkId and avatarId relationships.
  * <p>
  * When a new player logs in, they should connect with the chat service, registering that they are ready to participate
- * in the various chat services. Likewise, as their soe to the GameServer changes, the chat service should
+ * in the various chat services. Likewise, as their connection to the GameServer changes, the chat service should
  * potentially disconnect the player.
  */
 public interface AvatarService {
-    ChatAvatarId getSystemAvatarId();
-
-    ChatAvatarId lookupAvatarId(long networkId);
-
-    long lookupNetworkId(ChatAvatarId avatarId);
-
-    int getConnectionId(ChatAvatarId avatarId);
-
-    int getConnectionId(long networkId);
-
     /**
      * Connects a player to the chat service. This indicates that the player is ready to participate in the various
-     * chat services. When a player connects, other services will be notified of the soe.
+     * chat services. When a player connects, other services will be notified of the connection.
      *
-     * @param connectionId    The soe with which the player has connected to the game server.
-     * @param characterName The name of the character connecting.
+     * @param connectionId  The connection with which the player has connected to the game server.
+     * @param characterName The name of the character connecting. Should be just the first name.
      * @param networkId     The id of the character on the game server.
-     * @param isSecure      If the soe is using a secure protocol.
+     * @param isSecure      If the connection is made by an authorized god client.
      * @param isSubscribed  If the account is subscribed.
-     * @throws AvatarDoesNotExistException If the chat service believes the character is already connected, but the
-     *                                     disconnect attempt fails due to the chat service having no record of the avatar.
-     * @throws AvatarNotConnectedException If the chat service believes the character is already connected, but the
-     *                                     disconnect attempt fails due to the player not being connected.
      */
-    void connectPlayer(int connectionId, String characterName, long networkId, boolean isSecure, boolean isSubscribed)
-            throws AvatarDoesNotExistException, AvatarNotConnectedException;
+    void connectPlayer(int connectionId, String characterName, long networkId, boolean isSecure, boolean isSubscribed);
 
     /**
      * Disconnects a player from the chat service.
      *
      * @param networkId The networkId for the player. This id should be known to the chat service.
-     * @throws AvatarDoesNotExistException If the player is not known by the chat service.
-     * @throws AvatarNotConnectedException If the player is not connected to the chat service.
      */
-    void disconnectPlayer(long networkId) throws AvatarDoesNotExistException, AvatarNotConnectedException;
+    void disconnectPlayer(long networkId);
 
     /**
      * Disconnects a player via their avatarId from the chat service.
      *
      * @param avatarId The avatarId for the player. This id should be known to the chat service.
-     * @throws AvatarDoesNotExistException If the player is not known by the chat service.
-     * @throws AvatarNotConnectedException If the player is not connected to the chat service.
      */
-    void disconnectAvatar(ChatAvatarId avatarId) throws AvatarDoesNotExistException, AvatarNotConnectedException;
+    void disconnectAvatar(ChatAvatarId avatarId);
 }
