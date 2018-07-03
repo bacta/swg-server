44410a7da20d26a9cf3f63190f34590c5074044f
Chris Rush <chris@crrush.com>
2018-07-02 21:24:00 -0600

    Working implicit flow. Added custom login, register, and forgot-password pages.

---
+++ login/src/main/java/io/bacta/login/server/auth/AuthController.java
@@ -0,0 +1,91 @@
+package io.bacta.login.server.auth;
+
+import io.bacta.login.server.auth.model.ForgotPasswordRequest;
+import io.bacta.login.server.auth.model.UserRegistrationRequest;
+import io.bacta.login.server.data.BactaAccount;
+import io.bacta.login.server.repository.BactaAccountRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class AuthController {
+    private static final Logger LOG = LoggerFactory.getLogger(AuthController.class);
+    @Autowired
+    private BactaAccountRepository repository;
+
+    @Autowired
+    private PasswordEncoder userPasswordEncoder;
+
+    @GetMapping("/register")
+    public String register(Model model, String error) {
+        model.addAttribute("errorMsg", error);
+
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public RedirectView register(UserRegistrationRequest request) {
+        BactaAccount account = repository.findByUsername(request.getUsername());
+
+        if (account != null) {
+            return new RedirectView("/register?error=username-exists");
+        }
+
+        if (!request.getPassword().equals(request.getConfirmPassword())) {
+            return new RedirectView("/register?error=password-mismatch");
+        }
+
+        final String encodedPassword = userPasswordEncoder.encode(request.getPassword());
+        account = new BactaAccount(request.getUsername(), encodedPassword);
+
+        account = repository.save(account);
+
+        LOG.debug("Registered new bacta account (id: {}, username: {})",
+                account.getId(),
+                account.getUsername());
+
+        return new RedirectView("/register/success");
+    }
+
+    @GetMapping("/register/success")
+    public String registerSuccess() {
+        return "registration-success";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model, String error, String logout) {
+        if (error != null)
+            model.addAttribute("errorMsg", "Your username and password are invalid.");
+
+        if (logout != null)
+            model.addAttribute("msg", "You have been logged out successfully.");
+
+        return "login";
+    }
+
+    @GetMapping("/forgot-password")
+    public String forgotPassword(Model model, String success) {
+        if (success != null)
+            model.addAttribute("success", true);
+
+        return "forgot-password";
+    }
+
+    @PostMapping("/forgot-password")
+    public RedirectView forgotPassword(ForgotPasswordRequest request) {
+        return new RedirectView("/forgot-password?success");
+    }
+
+    @RequestMapping("/")
+    public String dashboard() {
+        return "index";
+    }
+}
--- login/src/main/java/io/bacta/login/server/auth/WebSecurityConfig.java
+++ login/src/main/java/io/bacta/login/server/auth/WebSecurityConfig.java
@@ -7,16 +7,17 @@ import org.springframework.context.annotation.Import;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity()
 @Order(1)
 @Import(Encoders.class)
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
 
@@ -30,7 +31,28 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(userPasswordEncoder);
+    protected void configure(AuthenticationManagerBuilder authBuilder) throws Exception {
+        authBuilder
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(userPasswordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                .antMatchers("/register*/**").permitAll()
+                .antMatchers("/forgot-password").permitAll()
+                .antMatchers("/oauth/token/revokeById/**").permitAll()
+                .antMatchers("/tokens/**").permitAll()
+                .antMatchers("/img/**", "/css/**", "/lib/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                    .formLogin()
+                    .loginPage("/login")
+                    //.failureForwardUrl("/login/error")
+                    .successForwardUrl("/")
+                    .permitAll()
+                .and()
+                    .csrf().disable();
     }
 }
