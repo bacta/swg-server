d8b5fa07ca5af3060af2a613560fddce41bd647c
Chris Rush <chris@crrush.com>
2018-09-11 18:41:00 -0600

    Fix names of files and other problems compiling.

--- login/src/main/java/io/bacta/login/server/session/SessionCredentialsInvalidException.java
+++
@@ -1,7 +0,0 @@
-package io.bacta.login.server.session;
-
-public final class SessionCredentialsInvalidException extends SessionException {
-    public SessionCredentialsInvalidException() {
-        super(String.format("The credentials provided were invalid and a session could not be established."));
-    }
-}
--- login/src/main/java/io/bacta/login/server/session/SessionException.java
+++
@@ -1,7 +0,0 @@
-package io.bacta.login.server.session;
-
-public class SessionException extends Exception {
-    public SessionException(String message) {
-        super(message);
-    }
-}
