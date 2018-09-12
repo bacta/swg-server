dd59bb8f5c2fa126a8689f68cd82027999a6074e
Kyle Burkhardt <mustangcoupe69@gmail.com>
2018-09-11 20:31:17 -0600

    [Fix] Failing game server test

--- game/src/test/groovy/io/bacta/game/GameServerApplicationSpecIT.groovy
+++ game/src/test/groovy/io/bacta/game/GameServerApplicationSpecIT.groovy
@@ -4,16 +4,18 @@ import com.codahale.metrics.MetricFilter
 import com.codahale.metrics.MetricRegistry
 import groovy.util.logging.Slf4j
 import io.bacta.engine.util.AwaitUtil
+import io.bacta.game.config.GameServerProperties
 import io.bacta.soe.network.connection.SoeConnection
 import io.bacta.soe.network.udp.SoeTransceiver
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Profile
 import spock.lang.Specification
 
 import javax.inject.Inject
 
 @Slf4j
 @SpringBootTest(classes = GameServerTestApplication.class)
+@Profile("test")
 class GameServerApplicationSpecIT extends Specification {
 
     @Inject
@@ -23,8 +25,9 @@ class GameServerApplicationSpecIT extends Specification {
     SoeTransceiver soeClient;
 
     String serverHost = "127.0.0.1"
-    @Value('${io.bacta.galaxy.server.bindPort}')
-    int serverPort
+
+    @Inject
+    GameServerProperties properties;
 
     void setup() {
         soeClient.start("Client")
@@ -39,14 +42,14 @@ class GameServerApplicationSpecIT extends Specification {
 
         setup:
 
-        SoeConnection connection = soeClient.getConnection(new InetSocketAddress(serverHost, serverPort))
+        SoeConnection connection = soeClient.getConnection(new InetSocketAddress(serverHost, properties.getBindPort()))
 
         when:
 
         connection.connect({udpConnection -> log.info("I connected")})
+        AwaitUtil.awaitTrue(connection.&isConnected, 5)
 
         then:
         noExceptionThrown()
-        AwaitUtil.awaitTrue(connection.&isConnected, 5)
     }
 }
