f7903320a5b73886410be7ecbe4c1e63b16c301d
Kyle Burkhardt <mustangcoupe69@gmail.com>
2018-09-13 00:08:59 -0600

    [Update] GalaxyServerOnline to implement Serializable until we do protobuffers?

--- shared/src/main/java/io/bacta/galaxy/message/GalaxyServerOnline.java
+++ shared/src/main/java/io/bacta/galaxy/message/GalaxyServerOnline.java
@@ -3,10 +3,12 @@ package io.bacta.galaxy.message;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.io.Serializable;
 import java.net.InetSocketAddress;
 
 @Getter
 @AllArgsConstructor
-public final class GalaxyServerOnline {
+public final class GalaxyServerOnline implements Serializable {
     private final InetSocketAddress address;
+
 }
