a3bbbdfc8629496d57c7486a438f6070c46540ae
Chris Rush <crush@nacmtampa.com>
2018-09-14 21:11:55 -0600

    LoginClusterStatus broadcast to active login server connections.

--- shared/src/main/java/io/bacta/game/message/connection/ClientIdMsg.java
+++ shared/src/main/java/io/bacta/game/message/connection/ClientIdMsg.java
@@ -10,15 +10,6 @@ import lombok.Getter;
 import java.nio.ByteBuffer;
 
 
-/**
- struct __cppobj ClientIdMsg : GameNetworkMessage
- {
-     Archive::AutoVariable<unsigned long> m_gameBitsToClear;
-     Archive::AutoArray<unsigned char> token;
-     Archive::AutoVariable<std::string > version;
-     char *tokenData;
- }; 
- */
 @Getter
 @Priority(0x4)
 @AllArgsConstructor
@@ -40,12 +31,4 @@ public class ClientIdMsg extends GameNetworkMessage {
         BufferUtil.putBinaryString(buffer, token);
         BufferUtil.putAscii(buffer, clientVersion);
     }
-    /**
-         04 00 26 92 89 D5 00 00 00 00 25 00 00 00 31 35
-    32 34 31 32 37 38 37 36 37 37 34 30 36 39 35 33
-    32 32 36 38 33 35 34 36 32 31 39 36 35 30 36 37
-    35 34 33 0E 00 32 30 30 35 31 30 31 30 2D 31 37
-    3A 30 30 
-
-     */
 }
