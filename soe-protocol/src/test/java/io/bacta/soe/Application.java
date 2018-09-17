bcc941f8d75307326ade71ef548aa2c586d6acf6
Kyle Burkhardt <mustangcoupe69@gmail.com>
2018-09-16 21:33:00 -0600

    Update template generation

---
+++ soe-protocol/src/test/java/io/bacta/soe/Application.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2017. Bacta
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package io.bacta.soe;
+
+import io.bacta.soe.network.dispatch.DefaultGameNetworkMessageDispatcher;
+import io.bacta.soe.network.dispatch.GameNetworkMessageControllerLoader;
+import io.bacta.soe.network.dispatch.GameNetworkMessageDispatcher;
+import io.bacta.soe.serialize.GameNetworkMessageSerializer;
+import io.bacta.soe.util.GameNetworkMessageTemplateWriter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+import javax.inject.Inject;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableConfigurationProperties
+@ComponentScan({"io.bacta.soe", "io.bacta.engine"})
+@PropertySources({
+        @PropertySource("classpath:soenetworking.properties"),
+        @PropertySource("classpath:application.properties")
+})
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Inject
+    @Bean
+    public GameNetworkMessageDispatcher getGameNetworkMessageDispatcher(final GameNetworkMessageControllerLoader controllerLoader,
+                                                                        final GameNetworkMessageSerializer gameNetworkMessageSerializer,
+                                                                        final GameNetworkMessageTemplateWriter gameNetworkMessageTemplateWriter) {
+
+        return new DefaultGameNetworkMessageDispatcher(controllerLoader, gameNetworkMessageSerializer, gameNetworkMessageTemplateWriter);
+    }
+}
