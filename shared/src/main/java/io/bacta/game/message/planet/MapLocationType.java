fccbd7b4a8675c82078e69ecfa634c01b8f71894
Chris Rush <chris@crrush.com>
2018-09-17 19:48:12 -0600

    Send of cached planet map. Needs more work.

--- shared/src/main/java/io/bacta/game/message/planet/MapLocationType.java
+++ shared/src/main/java/io/bacta/game/message/planet/MapLocationType.java
@@ -4,7 +4,16 @@ package io.bacta.game.message.planet;
  * Created by crush on 5/28/2016.
  */
 public enum MapLocationType {
+    /**
+     * Represents static objects.
+     */
     STATIC,
+    /**
+     * Represents dynamic objects.
+     */
     DYNAMIC,
-    PERSIST
+    /**
+     * Represents persistent objects (like houses).
+     */
+    PERSISTENT
 }
