faaf4c0762d4f74b1b07b4a60554a39a58cc430c
Chris Rush <chris@crrush.com>
2019-02-18 17:46:57 -0500

    World snapshot lading classes

---
+++ game/src/main/java/io/bacta/game/planet/WorldSnapshotDatabaseLoader.java
@@ -0,0 +1,52 @@
+package io.bacta.game.planet;
+
+import io.bacta.game.object.ServerObjectService;
+import io.bacta.swg.iff.Iff;
+import io.bacta.swg.snapshot.WorldSnapshot;
+import io.bacta.swg.snapshot.WorldSnapshotIffReader;
+import io.bacta.swg.snapshot.WorldSnapshotNode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+import java.util.List;
+
+/**
+ * Loads a world snapshot from the associated .ws IFF file, and then attempts to save it to the
+ * database. This can be used in conjunction with startup properties to only run on demand when a fresh
+ * snapshot needs to be imported. Any objects with existing object ids will be overwritten.
+ */
+@Slf4j
+@Service
+public class WorldSnapshotDatabaseLoader {
+    private final ServerObjectService serverObjectService;
+
+    @Inject
+    public WorldSnapshotDatabaseLoader(ServerObjectService serverObjectService) {
+        this.serverObjectService = serverObjectService;
+    }
+
+    public void load() {
+        //Find all the world snapshot files in a given directory?
+        //Attempt to load each with the world snapshot iff reader
+        //Serialize the objects to the database.
+
+        final String[] planets = new String[] {
+                "tatooine",
+                "naboo"
+        };
+
+        final WorldSnapshotIffReader iffReader = new WorldSnapshotIffReader();
+
+        for (final String planet : planets) {
+            final Iff iff = new Iff(); //TODO: Load external iff file.
+            final WorldSnapshot snapshot = iffReader.load(iff);
+
+            final List<WorldSnapshotNode> nodes = snapshot.getNodeList();
+
+            for (WorldSnapshotNode node : nodes) {
+                final long networkId = node.getNetworkId();
+            }
+        }
+    }
+}
