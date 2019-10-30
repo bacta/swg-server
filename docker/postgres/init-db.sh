71115eea1792960b4bbb448921f262f4fdd86ce2
Kyle Burkhardt <mustangcoupe69@gmail.com>
2019-10-29 18:48:24 -0600

    Add postgres docker

--- docker/postgres/init-db.sh
+++ docker/postgres/init-db.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" < /scripts/createuser.pgsql
+#psql -v ON_ERROR_STOP=1 -d bacta --username "$POSTGRES_USER" < /scripts/<sql file>.sql
